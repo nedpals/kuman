b2cc8d99a296bd26be1f6552d7d8c080158769f0
Ned Palacios <npdoesmc@gmail.com>
2019-01-25 07:56:52 +0800

    treat help and version options as commands

--- src/cli.ts
+++ src/cli.ts
@@ -56,6 +56,7 @@ export function runCli(argv: Array<string>, state: any) {
             value: "v",
             uppercase: false
         },
+        asCommand: true,
         description: "Displays CLI version"
     }, state);
 
@@ -66,7 +67,10 @@ export function runCli(argv: Array<string>, state: any) {
             value: "h",
             uppercase: false
         },
+        asCommand: true,
         description: "Displays the list of commands and options"
+    }, state);
+
     // Execute shorthand
     Object.keys(shorthandOptions).map(optName => {
         let currentArgOption = args.options[optName];
