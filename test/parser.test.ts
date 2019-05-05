afeb67ce3676e8d2bc372c6f7fe496528f71ecb7
Ned Palacios <npdoesmc@gmail.com>
2019-05-05 22:06:19 +0800

    update parser test code

--- test/parser.test.ts
+++ test/parser.test.ts
@@ -1,59 +1,61 @@
 import parseArgv from "../src/parser";
  
-test('parse argv', () => {
-    expect(parseArgv(["test", "--hello=world", "-F", "hey", "unkn"]))
-        .toEqual({
-            command: "test",
-            options: {
-                hello: "world",
-                F: "hey"
-            },
-            unknown: ["unkn"]
-        });
-});
-
-test('parse argv without command', () => {
-    expect(parseArgv(["--hello"]))
-        .toEqual({
-            command: undefined,
-            options: {
-                hello: ''
-            },
-            unknown: []
-        })
-});
-
-test('parse options with equals', () => {
-    expect(parseArgv(["--hello=world"]))
-        .toEqual({
-            command: undefined,
-            options: {
-                hello: "world"
-            },
-            unknown: []
-        })
-});
-
-test('parse options without equals', () => {
-    expect(parseArgv(["--hello", "world", "-F", "24", "--foo", "bar,net,que"]))
-        .toEqual({
-            command: undefined,
-            options: {
-                hello: "world",
-                F: "24",
-                foo: ["bar", "net", "que"]
-            },
-            unknown: []
-        })
-});
-
-test('parse options with an array of values', () => {
-    expect(parseArgv(["--hello=world,foo,bar"]))
-        .toEqual({
-            command: undefined,
-            options: {
-                hello: ["world", "foo", "bar"]
-            },
-            unknown: []
-        })
+describe("parse argv array", () => {
+    test("just parse", () => {
+        expect(parseArgv(["test", "--hello=world", "-F", "hey", "unkn"]))
+            .toEqual({
+                command: "test",
+                options: {
+                    hello: "world",
+                    F: "hey"
+                },
+                unknown: ["unkn"]
+            });
+    });
+    
+    test("parsing it without command", () => {
+        expect(parseArgv(["--hello"]))
+            .toEqual({
+                command: undefined,
+                options: {
+                    hello: ''
+                },
+                unknown: []
+            })
+    });
+    
+    test("parsing option value with equals", () => {
+        expect(parseArgv(["--hello=world"]))
+            .toEqual({
+                command: undefined,
+                options: {
+                    hello: "world"
+                },
+                unknown: []
+            })
+    });
+    
+    test("parsing set of option values without equals", () => {
+        expect(parseArgv(["--hello", "world", "-F", "24", "--foo", "bar,net,que"]))
+            .toEqual({
+                command: undefined,
+                options: {
+                    hello: "world",
+                    F: "24",
+                    foo: ["bar", "net", "que"]
+                },
+                unknown: []
+            })
+    });
+    
+    test("parsing comma-separated option value into an array of values", () => {
+        expect(parseArgv(["--hello=world,foo,bar"]))
+            .toEqual({
+                command: undefined,
+                options: {
+                    hello: ["world", "foo", "bar"]
+                },
+                unknown: []
+            })
+    });
 });
