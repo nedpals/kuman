21aeb1e251fb4f09c76c11d48bb0ebfe1c786055
Ned Palacios <npdoesmc@gmail.com>
2019-05-05 22:19:44 +0800

    change default description for shorthand vers.

--- src/help.ts
+++ src/help.ts
@@ -8,12 +8,12 @@ export default function generateHelp(state: any) {
     event.emit("showHelp");
 
     console.log("\nOPTIONS:\n");
-    
+
     let optionShorthands: Array<string> = state.options.map(o => o.shorthand).sort();
-        
+
     state.options.sort().map((option, i) => {
         let name = `--${option.name}${typeof optionShorthands[i] !== "undefined" ? (', -' + optionShorthands[i]) : ''}`;
-
+        
         console.log(`${name.padEnd(30, " ")}${option.description}`);
     });
 
@@ -21,7 +21,7 @@ export default function generateHelp(state: any) {
 
     state.commands.map(command => {
         let name = `${command.name}`;
-        let description = command.hasOwnProperty('parent') ? `Redirects to '${command.parent}' command.` : command.description;
+        let description = command.hasOwnProperty('parent') ? `Alias of '${command.parent}' command.` : command.description;
 
         console.log(name.padEnd(30, " ") + description);
     });
