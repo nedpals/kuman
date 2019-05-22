17d2442a360d8ea4fe689e760133851671692fd6
Ned Palacios <npdoesmc@gmail.com>
2019-05-22 14:59:41 +0800

    improve testing of cli option and commands

--- test/command.test.ts
+++ test/command.test.ts
@@ -1,13 +1,56 @@
 import mockConsole from "jest-mock-console";
 import { CLI } from "../kuman";
 
-describe("execute commands", () => {
-    test("run command", () => {
-        const restoreConsole = mockConsole();
-        const cli = CLI();
+const cli = CLI();
 
-        cli.command("hello", () => console.log("Hello"));
-        expect(cli.run(["hello"])).toHaveBeenCalled();
-        restoreConsole();
+describe("execute commands", () => {
+    let expectedFn;
+    let argv;
+
+    const cliRun = jest.spyOn(cli, "run");
+    const consoleLog = jest.spyOn(global.console, "log");
+
+    it("runs a command", () => {
+        expectedFn = () => console.log("Hello");
+        argv = ["hello"];
+
+        cli.command("hello", expectedFn);
+        cliRun.mockImplementation(expectedFn);
+
+        expect(cli.run(argv)).toBeUndefined();
+        expect(consoleLog).toHaveBeenCalledWith("Hello");
+        expect(cliRun).toHaveBeenCalledWith(argv);
+
+        cliRun.mockReset();
+        consoleLog.mockReset();
+    });
+
+    it("runs a command with required options", () => {
+        argv = ["print", "--age", "10"];
+
+        cli.option("age", (v) => v);
+        cli.command("print", ({ options }) => console.log(options.age), { requires: ["age"] });
+        cliRun.mockImplementation(() => console.log("10"));
+
+        expect(cli.run(argv)).toBeUndefined();
+        expect(consoleLog).toHaveBeenCalledWith("10");
+        expect(cliRun).toHaveBeenCalledWith(argv);
+
+        cliRun.mockReset();
+        consoleLog.mockReset();
+    });
+
+    it("runs a command with one argument", () => {
+        argv = ["name", "Ned"];
+
+        cli.command("name", ({ args }) => console.log(args.name), { arguments: 1 });
+        cliRun.mockImplementation(() => console.log("Ned"));
+
+        expect(cli.run(argv)).toBeUndefined();
+        expect(consoleLog).toHaveBeenCalledWith("Ned");
+        expect(cliRun).toHaveBeenCalledWith(argv);
+
+        cliRun.mockReset();
+        consoleLog.mockReset();
     });
 });
--- test/option.test.ts
+++ test/option.test.ts
@@ -1,5 +1,27 @@
-describe("test options", () => {
-    it("sets an option", () => {
-        console.log("TODO: Set option test");
+import mockConsole from "jest-mock-console";
+import { CLI } from "../kuman";
+
+const cli = CLI();
+
+describe("execute option", () => {
+    let expectedFn;
+    let argv;
+
+    const cliRun = jest.spyOn(cli, "run");
+    const consoleLog = jest.spyOn(global.console, "log");
+
+    it("runs a command", () => {
+        expectedFn = (v) => console.log(parseInt(v) + 10);
+        argv = ["--addOne", "10"];
+
+        cli.command("hello", expectedFn);
+        cliRun.mockImplementation(() => { console.log(11) });
+
+        expect(cli.run(argv)).toBeUndefined();
+        expect(consoleLog).toHaveBeenCalledWith(12);
+        expect(cliRun).toHaveBeenCalledWith(argv);
+
+        cliRun.mockReset();
+        consoleLog.mockReset();
     });
 });
